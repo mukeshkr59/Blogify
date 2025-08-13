e10b94f36ec2b6c0aed84d66db07cc7a89b88930
mukeshkr59 <mukeshkv20@iitk.ac.in>
2025-08-13 11:00:50 +0530

    final commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const {
 } = require("./middlewares/authentication");
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 mongoose
   .connect(process.env.MONGO_URL)
